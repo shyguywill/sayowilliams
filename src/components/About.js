8fb835f5313a9767a9fba039fb3f09003316dc68
Sayo Williams <sayo.williams@gmail.com>
2020-03-11 19:15:02 +0000

    add skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 
+
 class About extends React.Component {
     render() {
         const image = 'https://media.gq.com/photos/5963fe297bf9083f8bf87fcd/master/w_2000,h_2667,c_limit/john-boyega-burberry-close-up-0817-GQ-FEBO12-01.jpg'
@@ -13,7 +14,7 @@ class About extends React.Component {
                     
                     <div className="d-none d-md-block">
                         <div className="about">
-                            <div className="about-section" style={{backgroundImage: `url(${image})`, backgroundSize: 'cover', height: '80vh', borderRadius: '10px'}}>
+                            <div className="about-section" style={{backgroundImage: `url(${image})`, backgroundSize: 'cover', height: '80vh', borderRadius: '5px'}}>
                                 {/* <div className="profile-image" style={{backgroundImage: `url(${image})`, backgroundSize: 'cover'}}/> */}
 
                                 <div className="about-text">
@@ -25,9 +26,37 @@ class About extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="about-section"> 
-                                <div>Skills</div> 
-                                <div>Skills list</div>
+                            <div className="about-section" > 
+
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/react.png'} alt="logo" style={{height: '40px', width: '60px'}} />
+                                    {/* <div className="about-section" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/skills/react.png'})`, backgroundSize: 'contain', height: '50px', width: '50px'}}></div> */}
+                                    <div style={{fontSize: '30px', margin: '15px'}}>React Native</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/swift.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '25px', margin: '15px'}}>Swift/iOS</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/react.png'} alt="logo" style={{height: '40px', width: '60px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>React JS</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/js.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>Javascript</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/css.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>CSS</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/html.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '15px', margin: '15px'}}>HTML</div>
+                                </div>
+                                <div className="skills">
+                                    <img src={process.env.PUBLIC_URL + '/skills/dart.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '15px', margin: '15px'}}>Dart</div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -45,9 +74,36 @@ class About extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="about-section"> 
-                                <div>Skills</div> 
-                                <div>Skills list</div>
+                            <div className="mobile-column" style={{marginTop: '30px'}}> 
+                            <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/react.png'} alt="logo" style={{height: '40px', width: '60px'}} />
+                                    {/* <div className="about-section" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/skills/react.png'})`, backgroundSize: 'contain', height: '50px', width: '50px'}}></div> */}
+                                    <div style={{fontSize: '30px', margin: '15px'}}>React Native</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/swift.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '25px', margin: '15px'}}>Swift/iOS</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/react.png'} alt="logo" style={{height: '40px', width: '60px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>React JS</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/js.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>Javascript</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/css.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '20px', margin: '15px'}}>CSS</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/html.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '15px', margin: '15px'}}>HTML</div>
+                                </div>
+                                <div className="skills-mobile">
+                                    <img src={process.env.PUBLIC_URL + '/skills/dart.png'} alt="logo" style={{height: '40px', width: '40px'}} />
+                                    <div style={{fontSize: '15px', margin: '15px'}}>Dart</div>
+                                </div>
                             </div>
                         </div>
                     </div>
