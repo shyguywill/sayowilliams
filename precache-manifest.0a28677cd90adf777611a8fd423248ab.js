5d27ebc077166d245a22052df237ab29675c4503
Sayo Williams <sayo.williams@gmail.com>
2020-03-27 15:00:10 +0000

    Updates

---
+++ precache-manifest.0a28677cd90adf777611a8fd423248ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e330ce405eff86c8add593712ae912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd400236defbe4d8bd7",
+    "url": "/static/css/main.28237821.chunk.css"
+  },
+  {
+    "revision": "bff440579f0f893edd45",
+    "url": "/static/js/2.593527e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.593527e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd400236defbe4d8bd7",
+    "url": "/static/js/main.c5852c31.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
