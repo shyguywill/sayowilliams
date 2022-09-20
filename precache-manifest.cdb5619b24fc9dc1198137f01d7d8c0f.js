c48da267385e6141a847303d1799a898ab291b66
Sayo <43470214+shyguywill@users.noreply.github.com>
2022-09-20 18:03:24 +0100

    Update precache-manifest.cdb5619b24fc9dc1198137f01d7d8c0f.js

--- precache-manifest.cdb5619b24fc9dc1198137f01d7d8c0f.js
+++ precache-manifest.cdb5619b24fc9dc1198137f01d7d8c0f.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "90104663b467ca8c144168c48d592101",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "ba29b3e2b90c989f370f",
-    "url": "/static/css/main.fc4b221c.chunk.css"
+    "url": "./static/css/main.fc4b221c.chunk.css"
   },
   {
     "revision": "c08d2ee0b9d13cccb07b",
-    "url": "/static/js/2.d546482a.chunk.js"
+    "url": "./static/js/2.d546482a.chunk.js"
   },
   {
     "revision": "282cad4058e83221804611932e7ca1ed",
-    "url": "/static/js/2.d546482a.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.d546482a.chunk.js.LICENSE.txt"
   },
   {
     "revision": "ba29b3e2b90c989f370f",
-    "url": "/static/js/main.6b69b546.chunk.js"
+    "url": "./static/js/main.6b69b546.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
-    "url": "/static/js/runtime-main.19ccc7f6.js"
+    "url": "./static/js/runtime-main.19ccc7f6.js"
   }
 ]);
